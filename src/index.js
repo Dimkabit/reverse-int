32a51174f4f279b0b44017174f8eaf4268872117
Dimkabit <bit.82@mail.ru>
2020-10-21 18:53:15 +0300

    init: task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+	num = n + '';
+        let reversedText = num.split('').reverse().join('');
+        let reversedNumber = parseInt(reversedText, 10);
+    
+        return reversedNumber;               
 }
